8713b95d8060a3de748bc91ff99c16fba4594c76
Dmitrii Stebliuk <dstebliuk@gmail.com>
2020-05-01 13:44:16 +0200

    Using Picasso for image loading

--- app/src/main/java/ru/dsteb/popmovies/ImdbAdapter.java
+++ app/src/main/java/ru/dsteb/popmovies/ImdbAdapter.java
@@ -1,5 +1,6 @@
 package ru.dsteb.popmovies;
 
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,14 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 
 public class ImdbAdapter extends RecyclerView.Adapter<ImdbAdapter.ImbdbViewHolder> {
 
     private static final String TAG = ImdbAdapter.class.getCanonicalName();
+
+    private static final String IMG_URL = "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,704,1000_AL_.jpg";
 
     private final String[] mDataset = new String[100];
 
@@ -22,13 +27,16 @@ public class ImdbAdapter extends RecyclerView.Adapter<ImdbAdapter.ImbdbViewHolde
         boolean attachToRoot = false;
         View layout = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.view_holder_item, parent, attachToRoot);
-        Log.d(TAG, "onCreateViewHolder: ");
         return new ImbdbViewHolder(layout);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ImbdbViewHolder holder, int position) {
-
+        Picasso
+                .get()
+                .load(Uri.parse(IMG_URL))
+                .placeholder(R.drawable.demo)
+                .into(holder.itemView);
     }
 
     @Override
